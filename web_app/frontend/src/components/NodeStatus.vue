ef19e54de7abbb1529f79cc4bf9f7383d1b778c1
homirun <hatunemiku.moco@gmail.com>
2020-11-10 05:04:35 +0900

    フロントにおけるapiからのnode_statusの取得の実装

---
+++ web_app/frontend/src/components/NodeStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="node_status_view">
+    {{ results }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'NodeStatus',
+  data: function () {
+    return {
+      results: null
+    }
+  },
+  mounted() {
+      axios.get('http://localhost:5500/nodes_status').then(response => {
+        this.results = response;
+        console.log(this.results)
+      }).catch( error => { console.log(error); });
+  }
+}
+</script>
