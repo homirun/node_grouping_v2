908aa1eff5ee3c6553961cf47f431745e6f082de
homirun <hatunemiku.moco@gmail.com>
2020-11-10 15:53:55 +0900

    バックエンドへのポーリングとリアクティブ更新の実装

--- node_grouping/demo_api_server.py
+++ node_grouping/demo_api_server.py
@@ -14,22 +14,31 @@ app = Flask(__name__)
 @app.route('/api/nodes_status', methods=['get'])
 def nodes_status():
     node_list = get_node_list()
-    return json.dumps(node_list)
+
+    return json.dumps(grouping(node_list, 3))
 
 
 def get_node_list():
     # TODO: gRPCで稼働中のノードから受け取る
-    request_ip = '172.16.124.1'
-    with grpc.insecure_channel(request_ip + ':50051') as channel:
-        stub = node_pb2_grpc.RequestServiceStub(channel)
-        request_message = node_pb2.AddRequestDef(request_id=create_request_id(), time_stamp=get_now_unix_time())
-        response = stub.nodes_status_request(request_message)
-    response_node_list = list()
-    for node in response.node_list:
-        tmp_node = Node(uid=node.id, ip=node.ip, boot_time=node.boot_time, group_id=node.group_id,
-                        is_leader=node.is_leader)
-        response_node_list.append(tmp_node.__dict__)
-    return response_node_list
+    try:
+        request_ip = '172.16.124.1'
+        with grpc.insecure_channel(request_ip + ':50051') as channel:
+            stub = node_pb2_grpc.RequestServiceStub(channel)
+            request_message = node_pb2.AddRequestDef(request_id=create_request_id(), time_stamp=get_now_unix_time())
+            response = stub.nodes_status_request(request_message)
+    except grpc.RpcError:
+        request_ip = '172.16.124.10'
+        with grpc.insecure_channel(request_ip + ':50051') as channel:
+            stub = node_pb2_grpc.RequestServiceStub(channel)
+            request_message = node_pb2.AddRequestDef(request_id=create_request_id(), time_stamp=get_now_unix_time())
+            response = stub.nodes_status_request(request_message)
+    finally:
+        response_node_list = list()
+        for node in response.node_list:
+            tmp_node = Node(uid=node.id, ip=node.ip, boot_time=node.boot_time, group_id=node.group_id,
+                            is_leader=node.is_leader)
+            response_node_list.append(tmp_node.__dict__)
+        return response_node_list
 
 
 if __name__ == '__main__':
--- web_app/frontend/src/components/NodeStatus.vue
+++ web_app/frontend/src/components/NodeStatus.vue
@@ -1,6 +1,8 @@
 <template>
+  {{ results }}
   <div id="node_status_view">
-    {{ results }}
+
+    <div class="group" v-for="n in results" :class="['group'+n.group_id, n.is_leader ? 'is_leader' : '']"  :key="n">{{n}}</div>
   </div>
 </template>
 
@@ -14,10 +16,51 @@ export default {
     }
   },
   mounted() {
-      axios.get('http://localhost:5500/nodes_status').then(response => {
-        this.results = response;
-        console.log(this.results)
-      }).catch( error => { console.log(error); });
+    let me = this
+    axios.get('http://localhost:5500/nodes_status').then(function(response) {
+      me.results = response.data;
+      console.log(me.results)
+    }).catch(function(error){ console.log(error); });
+    setInterval(function() {
+      axios.get('http://localhost:5500/nodes_status').then(function(response){
+        me.results = response.data;
+      console.log(me.results)
+    }).catch(function(error){ console.log(error);});
+  }, 1000);
   }
 }
+
 </script>
+
+<style>
+#node_status_view{
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.group{
+  background-color: #42b983;
+  margin: 2rem;
+  width: 30rem;
+}
+
+.group1{
+  background-color: lightpink;
+}
+
+.group2{
+  background-color: lightgreen;
+}
+
+.group3{
+
+}
+
+.is_leader{
+  border: #F44336 solid 3px;
+}
+
+</style>
